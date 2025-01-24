8dc11967621a7caade31318819253f9913740339
John Gorski <john.gorski.jr@gmail.com>
2025-01-24 11:29:37 -0800

    Allow mermaid graphs to accept direction parameters
    Sometimes task dependency graphs are wide, sometimes they're tall.

--- src/pert/mermaid.clj
+++ src/pert/mermaid.clj
@@ -6,26 +6,39 @@
 (defn mermaid-string [s] (str "\"" (string/escape s {\" "&quot;"}) "\""))
 (defn mermaid-round [text] (str "(" (mermaid-string text) ")"))
 
+(defn mermaid-graph-direction [from to]
+  (let [default "RL"
+        ;; turns out there aren't any diagonal graph directions
+        dir-representations {:top "TB" ;; to bottom
+                             :bottom "BT" ;; to top
+                             :left "LR" ;; to right
+                             :right "RL" ;; to left
+                             }]
+    (get dir-representations from default)))
+
 (defn graph
   "Mermaid graph syntax for graph defined on tasks from pert.graph namespace."
-  [graph]
-  (let [{:keys [vertices edges]} graph
-        vertex-lookup (into {} (map (fn [v] [(:id v) (dissoc v :id)])) vertices)
-        mermaid-lines (concat
-                       (map (fn [[id {:keys [label]}]]
-                              (str id (mermaid-round label)))
-                            vertex-lookup)
-                       (map (fn [[from to]]
-                              (str
-                               from
-                               (mermaid-round (get-in vertex-lookup [from :label]))
-                               " --> "
-                               to
-                               (mermaid-round (get-in vertex-lookup [to :label]))
-                               )) edges))
-        ]
-    (string/join "\n" (concat ["graph RL"] (map (fn [line] (str "    " line)) mermaid-lines)) )
-    ))
+  ([g] (graph {:direction [:right :left]} g))
+  ([props graph]
+   (let [{:keys [direction]} props
+         dir (apply mermaid-graph-direction direction)
+         {:keys [vertices edges]} graph
+         vertex-lookup (into {} (map (fn [v] [(:id v) (dissoc v :id)])) vertices)
+         mermaid-lines (concat
+                        (map (fn [[id {:keys [label]}]]
+                               (str id (mermaid-round label)))
+                             vertex-lookup)
+                        (map (fn [[from to]]
+                               (str
+                                from
+                                (mermaid-round (get-in vertex-lookup [from :label]))
+                                " --> "
+                                to
+                                (mermaid-round (get-in vertex-lookup [to :label]))
+                                )) edges))
+         ]
+     (string/join "\n" (concat [(str "graph " dir)] (map (fn [line] (str "    " line)) mermaid-lines)) )
+     )))
 
 (def render-fn
   '(fn [value]
